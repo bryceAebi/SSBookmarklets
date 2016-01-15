8a16ffcc0485d75335c92f701ec2001d195dd350
Bryce Aebi <bryce.a.aebi@gmail.com>
2016-01-15 14:45:14 -0800

    Update ccfill.js

--- ccfill.js
+++ ccfill.js
@@ -21,11 +21,11 @@
 	
 	function initMyBookmarklet() {
 		(window.myBookmarklet = function() {
-			$('#id_payment_card_number').text("5555555555554444");
-			$('#id_payment_card_cvv2').text("123");
-			$('.add-credit-card-simple-zipcode').text("12345");
-			$('#id_payment_card_year').val('2020');
-			$('#id_payment_card_month').val('02');
+			$j('#id_payment_card_number').text("5555555555554444");
+			$j('#id_payment_card_cvv2').text("123");
+			$j('.add-credit-card-simple-zipcode').text("12345");
+			$j('#id_payment_card_year').val('2020');
+			$j('#id_payment_card_month').val('02');
 		})();
 	}
 
